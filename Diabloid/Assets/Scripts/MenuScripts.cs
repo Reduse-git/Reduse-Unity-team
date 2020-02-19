f49d8b7940dbdadaddafa46af47dce4a92d48d91
Reduse <den.reduse@gmail.com>
2020-02-19 11:54:42 +0200

    Init menu

---
+++ Diabloid/Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void OnClickSetting()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+    public void OnClickExit()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+    public void OnClickPause()
+    {
+        SceneManager.LoadScene("SceneMenu");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
